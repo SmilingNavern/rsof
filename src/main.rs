d9fba656e94ecec9da05d75acab5062daa1c10e0
alexander.danilov <livingdeadzerg@yandex.ru>
2017-07-05 20:06:34 +0300

    Show uids

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,49 @@
+extern crate users;
+use users::{Users, Groups, UsersCache};
 extern crate psutil;
+#[macro_use]
+extern crate clap;
+use clap::{Arg, App, SubCommand, ArgMatches};
 
 
-fn main() {
-    for p in psutil::process::all().unwrap() {
-        if let Ok(ref fds) = p.open_fds() {
-            for fd in fds {
-                 println!("{} {} {}fd {}", p.pid, p.uid, fd.number, fd.path.as_path().display())
-            }
+fn show_process_fds(process: psutil::process::Process, cache: &mut UsersCache) {
+    let user_name = cache.get_user_by_uid(process.uid).name();
+    if let Ok(fds) = process.open_fds() {
+        for fd in fds {
+            println!("{} {} {}fd {}", process.pid, user_name, fd.number, fd.path.as_path().display())
         }
     }
 
+}
+
+
+fn show_all_process_fds(cache: &mut UsersCache) {
+    for p in psutil::process::all().unwrap() {
+        show_process_fds(p, &mut cache);
+    }
+}
+
+
+fn main() {
+    let matches = App::new("rsof")
+                          .version("0.1.0")
+                          .author("Alexander D <livingdeadzerg@yandex.ru>")
+                          .arg(Arg::with_name("pid")
+                               .short("p")
+                               .long("pid")
+                               .value_name("PID")
+                               .help("Pid of process")
+                               .takes_value(true))
+                          .get_matches();
+
+    let mut cache = UsersCache::new();
+    if let Ok(pid) = value_t!(matches, "pid", i32) {
+        //let process = psutil::process::Process::new(pid).unwrap();
+        let _ = match psutil::process::Process::new(pid) {
+            Ok(process) => show_process_fds(process, &mut cache),
+            Err(_) => println!("Error getting fds for process {}", pid),
+        };
+    } else {
+        show_all_process_fds(&mut cache);
+    }
 }
